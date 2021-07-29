91cdbbebc78cf6d9b3bcaaaf3d7ba8ff5f85a528
tom doron <tomer@apple.com>
2021-07-29 13:16:49 -0700

    split events into spearate package
    motivation: the runtime library has a stabel API while the events are still moving target, so in order to provide a 1.0 stable version we should separate them out
    
    changes:
    * import events module from the swift-aws-lambda-runtime, including git history
    * update docker setup
    * remove redundant examples and  scripts

--- Package.swift
+++ Package.swift
@@ -3,56 +3,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-aws-lambda-runtime",
+    name: "swift-aws-lambda-events",
     products: [
-        // this library exports `AWSLambdaRuntimeCore` and adds Foundation convenience methods
-        .library(name: "AWSLambdaRuntime", targets: ["AWSLambdaRuntime"]),
-        // this has all the main functionality for lambda and it does not link Foundation
-        .library(name: "AWSLambdaRuntimeCore", targets: ["AWSLambdaRuntimeCore"]),
-        // common AWS events
         .library(name: "AWSLambdaEvents", targets: ["AWSLambdaEvents"]),
-        // for testing only
-        .library(name: "AWSLambdaTesting", targets: ["AWSLambdaTesting"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.30.0")),
-        .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.4.2")),
-        .package(url: "https://github.com/swift-server/swift-backtrace.git", .upToNextMajor(from: "1.2.3")),
-    ],
+    dependencies: [],
     targets: [
-        .target(name: "AWSLambdaRuntime", dependencies: [
-            .byName(name: "AWSLambdaRuntimeCore"),
-            .product(name: "NIO", package: "swift-nio"),
-            .product(name: "NIOFoundationCompat", package: "swift-nio"),
-        ]),
-        .target(name: "AWSLambdaRuntimeCore", dependencies: [
-            .product(name: "Logging", package: "swift-log"),
-            .product(name: "Backtrace", package: "swift-backtrace"),
-            .product(name: "NIOHTTP1", package: "swift-nio"),
-            .product(name: "_NIOConcurrency", package: "swift-nio"),
-        ]),
-        .testTarget(name: "AWSLambdaRuntimeCoreTests", dependencies: [
-            .byName(name: "AWSLambdaRuntimeCore"),
-            .product(name: "NIOTestUtils", package: "swift-nio"),
-            .product(name: "NIOFoundationCompat", package: "swift-nio"),
-        ]),
-        .testTarget(name: "AWSLambdaRuntimeTests", dependencies: [
-            .byName(name: "AWSLambdaRuntimeCore"),
-            .byName(name: "AWSLambdaRuntime"),
-        ]),
         .target(name: "AWSLambdaEvents", dependencies: []),
         .testTarget(name: "AWSLambdaEventsTests", dependencies: ["AWSLambdaEvents"]),
-        // testing helper
-        .target(name: "AWSLambdaTesting", dependencies: [
-            .byName(name: "AWSLambdaRuntime"),
-            .product(name: "NIO", package: "swift-nio"),
-        ]),
-        .testTarget(name: "AWSLambdaTestingTests", dependencies: ["AWSLambdaTesting"]),
-        // for perf testing
-        .target(name: "MockServer", dependencies: [
-            .product(name: "NIOHTTP1", package: "swift-nio"),
-        ]),
-        .target(name: "StringSample", dependencies: ["AWSLambdaRuntime"]),
-        .target(name: "CodableSample", dependencies: ["AWSLambdaRuntime"]),
     ]
 )
