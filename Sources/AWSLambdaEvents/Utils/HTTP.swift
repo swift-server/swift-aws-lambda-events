92ca5688e903cf87add918a030a58b778da0fff7
Mahdi Bahrami <github@mahdibm.com>
2023-06-29 14:41:06 +0330

    add `HTTPHeaders.first(name:)`

--- Sources/AWSLambdaEvents/Utils/HTTP.swift
+++ Sources/AWSLambdaEvents/Utils/HTTP.swift
@@ -12,10 +12,68 @@
 //
 //===----------------------------------------------------------------------===//
 
-// MARK: HTTPMethod
+// MARK: HTTPHeaders
 
 public typealias HTTPHeaders = [String: String]
 public typealias HTTPMultiValueHeaders = [String: [String]]
+
+extension HTTPHeaders {
+    /// Retrieves the first value for a given header-field / dictionary-key (`name`) from the block.
+    /// This method uses case-insensitive comparisons.
+    ///
+    /// - Parameter name: The header field name whose first value should be retrieved.
+    /// - Returns: The first value for the header field name.
+    public func first(name: String) -> String? {
+        guard !self.isEmpty else {
+            return nil
+        }
+
+        return self.first { header in header.0.isEqualCaseInsensitiveASCIIBytes(to: name) }?.1
+    }
+}
+
+extension String {
+    internal func isEqualCaseInsensitiveASCIIBytes(to: String) -> Bool {
+        return self.utf8.compareCaseInsensitiveASCIIBytes(to: to.utf8)
+    }
+}
+
+extension Sequence where Self.Element == UInt8 {
+    /// Compares the collection of `UInt8`s to a case insensitive collection.
+    ///
+    /// This collection could be get from applying the `UTF8View`
+    ///   property on the string protocol.
+    ///
+    /// - Parameter bytes: The string constant in the form of a collection of `UInt8`
+    /// - Returns: Whether the collection contains **EXACTLY** this array or no, but by ignoring case.
+    internal func compareCaseInsensitiveASCIIBytes<T: Sequence>(to: T) -> Bool
+    where T.Element == UInt8 {
+        // fast path: we can get the underlying bytes of both
+        let maybeMaybeResult = self.withContiguousStorageIfAvailable { lhsBuffer -> Bool? in
+            to.withContiguousStorageIfAvailable { rhsBuffer in
+                if lhsBuffer.count != rhsBuffer.count {
+                    return false
+                }
+
+                for idx in 0 ..< lhsBuffer.count {
+                    // let's hope this gets vectorised ;)
+                    if lhsBuffer[idx] & 0xdf != rhsBuffer[idx] & 0xdf {
+                        return false
+                    }
+                }
+                return true
+            }
+        }
+
+        if let maybeResult = maybeMaybeResult, let result = maybeResult {
+            return result
+        } else {
+            return self.elementsEqual(to, by: {return ($0 & 0xdf) == ($1 & 0xdf)})
+        }
+    }
+}
+
+// MARK: HTTPMethod
 
 public struct HTTPMethod: RawRepresentable, Equatable {
     public var rawValue: String
