6bbd2874e97c39588eab02173fef58d3b35e2d72
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2024-12-20 10:25:14 +0100

    add an encodable version of the APIGatewayResponse

---
+++ Sources/AWSLambdaEvents/APIGateway+V2+Encodable.swift
@@ -0,0 +1,50 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftAWSLambdaRuntime open source project
+//
+// Copyright (c) 2017-2022 Apple Inc. and the SwiftAWSLambdaRuntime project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftAWSLambdaRuntime project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+#if canImport(FoundationEssentials)
+import class FoundationEssentials.JSONEncoder
+import class FoundationEssentials.Data
+#else
+import class Foundation.JSONEncoder
+import struct Foundation.Data
+#endif
+
+import HTTPTypes
+
+public enum APIGatewayResponseError: Error {
+    case failedToEncodeBody(Error)
+}
+
+extension APIGatewayV2Response {
+
+    public init<Input: Encodable> (
+        statusCode: HTTPResponse.Status,
+        headers: HTTPHeaders? = nil,
+        body: Input,
+        isBase64Encoded: Bool? = nil,
+        cookies: [String]? = nil
+    ) throws {
+        let encodedBody: Data
+        do {
+            encodedBody = try JSONEncoder().encode(body)
+        } catch {
+            throw APIGatewayResponseError.failedToEncodeBody(error)
+        }
+        self.statusCode = statusCode
+        self.headers = headers
+        self.body = String(data: encodedBody, encoding: .utf8) ?? ""
+        self.isBase64Encoded = isBase64Encoded
+        self.cookies = cookies
+    }
+}
---
+++ Tests/AWSLambdaEventsTests/APIGateway+V2+EncodableTests.swift
@@ -0,0 +1,53 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftAWSLambdaRuntime open source project
+//
+// Copyright (c) 2017-2020 Apple Inc. and the SwiftAWSLambdaRuntime project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftAWSLambdaRuntime project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Testing
+
+import Foundation 
+
+@testable import AWSLambdaEvents
+
+struct APIGatewayV2EncodableResponseTests {
+
+
+    // MARK: Encoding
+    struct BusinessResponse: Codable, Equatable {
+        let message: String
+        let code: Int
+    }
+
+    @Test 
+    func testResponseEncoding() throws {
+
+        // given 
+        let businessResponse = BusinessResponse(message: "Hello World", code: 200)
+        
+        var response: APIGatewayV2Response? = nil
+        #expect(throws: Never.self) { 
+            try response = APIGatewayV2Response(statusCode: .ok, body: businessResponse)
+        }        
+        try #require(response?.body != nil)
+
+        // when 
+        let body = response?.body?.data(using: .utf8) 
+        try #require(body != nil)
+
+        #expect(throws: Never.self) { 
+            let encodedBody = try JSONDecoder().decode(BusinessResponse.self, from: body!)
+            
+            // then
+            #expect(encodedBody == businessResponse)
+        }
+    }
+}
