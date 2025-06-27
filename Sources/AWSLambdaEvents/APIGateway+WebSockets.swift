1dea37ef0e452d122a51662c00360545f6614c73
Richard Kendall Wolf <richwolf@me.com>
2025-06-26 20:20:04 -0500

    Make context properties public

--- Sources/AWSLambdaEvents/APIGateway+WebSockets.swift
+++ Sources/AWSLambdaEvents/APIGateway+WebSockets.swift
@@ -18,23 +18,23 @@ public struct APIGatewayWebSocketRequest: Codable {
     /// `Context` contains information to identify the AWS account and resources invoking the Lambda function.
     public struct Context: Codable {
         public struct Identity: Codable {
-            let sourceIp: String
+            public let sourceIp: String
         }
 
-        let routeKey: String
-        let eventType: String
-        let extendedRequestId: String
+        public let routeKey: String
+        public let eventType: String
+        public let extendedRequestId: String
         /// The request time in format: 23/Apr/2020:11:08:18 +0000
-        let requestTime: String
-        let messageDirection: String
-        let stage: String
-        let connectedAt: UInt64
-        let requestTimeEpoch: UInt64
-        let identity: Identity
-        let requestId: String
-        let domainName: String
-        let connectionId: String
-        let apiId: String
+        public let requestTime: String
+        public let messageDirection: String
+        public let stage: String
+        public let connectedAt: UInt64
+        public let requestTimeEpoch: UInt64
+        public let identity: Identity
+        public let requestId: String
+        public let domainName: String
+        public let connectionId: String
+        public let apiId: String
     }
 
     public let headers: HTTPHeaders?
